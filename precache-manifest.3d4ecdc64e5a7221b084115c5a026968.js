289dae02f12ce40f9f09693db4ccd8cd98e345f1
Ebrahim Khalil <ebrahimkhalilamidbd@gmail.com>
2020-02-19 23:32:16 +0600

    Updates

---
+++ precache-manifest.3d4ecdc64e5a7221b084115c5a026968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfa129087378919df64358e553f2506",
+    "url": "/react-bookstore-cart/index.html"
+  },
+  {
+    "revision": "b4618cf0a32cad9c02db",
+    "url": "/react-bookstore-cart/static/css/main.9c7dfb6a.chunk.css"
+  },
+  {
+    "revision": "4642b805af1e41dcda23",
+    "url": "/react-bookstore-cart/static/js/2.60348500.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-bookstore-cart/static/js/2.60348500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4618cf0a32cad9c02db",
+    "url": "/react-bookstore-cart/static/js/main.4ee6258a.chunk.js"
+  },
+  {
+    "revision": "79c08841c863220d0bb5",
+    "url": "/react-bookstore-cart/static/js/runtime-main.e607833a.js"
+  }
+]);
